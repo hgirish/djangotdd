d0e6e53d566f8ff93119e0446dba99a90dbaceb3
Girish <girish@post.com>
2017-08-25 08:22:23 -0700

    autoformat functional_tests

--- functional_tests.py
+++ functional_tests.py
@@ -3,15 +3,15 @@ import unittest
 import time
 from selenium.webdriver.common.keys import Keys
 
+
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
-    
+
     def tearDown(self):
         self.browser.quit()
-    
-    def test_can_start_a_list_and_retrieve_it_later(self):
 
+    def test_can_start_a_list_and_retrieve_it_later(self):
 
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
@@ -21,10 +21,11 @@ class NewVisitorTest(unittest.TestCase):
         header_text = self.browser.find_element_by_tag_name('h1').text()
         self.assertIn('To-Do', header_text)
 
-        
         # She is invited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(inputbox.get_attribute('placeholder'),'Enter a to do item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to do item')
         # She types "Buy peacock feathers" into a text box (Edith's hobby
         # is tying fly-fishing lures)
         inputbox.send_keys('Buy peacock feathers')
@@ -34,8 +35,9 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         table = self.browser.find_element_by_id('id_list_table')
-        rows  = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows))
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows))
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
         # The page updates again, and now shows both items on her list
