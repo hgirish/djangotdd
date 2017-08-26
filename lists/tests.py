eb742fd1e8ddd68cb9ba536bc81d57e69ed0f30e
hgirish <girish@post.com>
2017-08-25 18:44:22 -0700

    removed ununsed imports from tests

--- lists/tests.py
+++ lists/tests.py
@@ -1,8 +1,4 @@
 from django.test import TestCase
-from django.urls import resolve
-from lists.views import home_page
-from django.http import HttpRequest
-from django.template.loader import render_to_string
 
 
 class HomePageTest(TestCase):
