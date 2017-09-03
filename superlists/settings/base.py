7a32b5e9d95c3713e9258dbb2d99fc4a03b4897e
hgirish <girish@post.com>
2017-09-03 16:55:10 -0700

    changed path of base dir

--- superlists/settings/base.py
+++ superlists/settings/base.py
@@ -14,7 +14,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+# since we added settings directory, we need to go one level up
+BASE_DIR = os.path.dirname(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
