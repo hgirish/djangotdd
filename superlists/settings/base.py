cc43db6667e65684ab9eff98125789aa514bef30
hgirish <girish@post.com>
2017-09-04 00:15:26 -0700

    added secret key generator

--- superlists/settings/base.py
+++ superlists/settings/base.py
@@ -11,7 +11,15 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+from django.core.exceptions import ImproperlyConfigured
 
+def get_env_variable(var_name):
+    try:
+        return os.environ[var_name]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(var_name)
+        raise ImproperlyConfigured(error_msg)
+    
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # since we added settings directory, we need to go one level up
@@ -21,8 +29,30 @@ BASE_DIR = os.path.dirname(BASE_DIR)
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'rfw$s!@_oz2yz_&mq*y9v#@kd$q9_2vm4o5-*g_=2x#5$4zng)'
-
+#SECRET_KEY = get_env_variable('SECRET_KEY')
+#SECRET_KEY = 'rfw$s!@_oz2yz_&mq*y9v#@kd$q9_2vm4o5-*g_=2x#5$4zng)'
+try:
+    SECRET_KEY
+except NameError as ex:
+    print('NameError', ex)
+    SECRET_FILE = os.path.join(BASE_DIR, 'secret.txt')
+    try:
+        SECRET_KEY = open(SECRET_FILE).read().strip()
+    except IOError as ex:
+        print('IOError', ex)
+        SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+        
+        if SECRET_KEY is None:
+            print('SECRET_KEY is not present in environment ')
+            try:
+                import random
+                SECRET_KEY = ''.join([random.SystemRandom().choice('abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)') for i in range(50)])
+                secret = open(SECRET_FILE, 'w')
+                secret.write(SECRET_KEY)
+                secret.close()
+            except IOError:
+                Exception('Please create a %s file with random characters     to generate your secret key!' % SECRET_FILE)
+print('SECRET_KEY: {}'.format(SECRET_KEY))                
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
