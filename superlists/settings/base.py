b00096124c88fe93cc757b9e8f8abb8cd4715920
Girish <girish@post.com>
2017-09-07 12:52:11 -0700

    comment print database path

--- superlists/settings/base.py
+++ superlists/settings/base.py
@@ -117,7 +117,7 @@ DATABASES = {
         'NAME': dbpath,
     }
 }
-print('database path', dbpath)
+#print('database path', dbpath)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
