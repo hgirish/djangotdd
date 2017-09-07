6f6a43a15a34a8446a33e3c69da4289d951b4fad
Girish <girish@post.com>
2017-09-07 13:22:27 -0700

    fixed type in view

--- accounts/views.py
+++ accounts/views.py
@@ -12,7 +12,7 @@ def send_login_email(request):
     url = request.build_absolute_uri(
         reverse('login') + '?token=' + str(token.uid)
     )
-    message_body = "Use this link to log in: \m\n{}".format(url)
+    message_body = "Use this link to log in: \n\n{}".format(url)
     send_mail(
         'Your login link for Superlists',    
         message_body,    
